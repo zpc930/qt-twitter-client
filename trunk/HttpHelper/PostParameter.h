7d376d7ad3d7a578ab3f9ad8837f8f08b5c0da0e
lvjin.buaa@gmail.com <lvjin.buaa@gmail.com@7c53731f-62f4-5332-f4eb-b6b0b6567c15>
2011-05-17 17:06:15 +0000

    http by lvjin

--- trunk/HttpHelper/PostParameter.h
+++ trunk/HttpHelper/PostParameter.h
@@ -6,6 +6,7 @@
 #ifndef __POST_PARAMETER__
 #define __POST_PARAMETER__
 
+#include <QtCore/QList>
 #include <string>
 #define POST_NAME_LEN  64
 #define POST_VALUE_LEN 1024
@@ -16,45 +17,25 @@ using namespace std;
 class PostParameter
 {
 private:
-    string name;
-    string value;
+    char* name;
+    char* value;
     FILE file;
 public:
     PostParameter();
-    PostParameter(char* name, char* value);
-    PostParameter(char* name, int value);
-    PostParameter(char* name, double value);
-    PostParameter(char* name, FILE file);
+    PostParameter(const char* name, const char* value);
+    PostParameter(const char* name, const int value);
+    PostParameter(const char* name, const double value);
+    //PostParameter(char* name, FILE file);
     ~PostParameter();
 
     char* getName() const;
     char* getValue() const;
-    static char* generatePostParameter(PostParameter *paras, int count);
+    void setName(const char* name);
+    void setValue(const char* value);
+    void setValue(const int value);
+    void setValue(const double value);
+    static char* generatePostParameter(QList<PostParameter*> &paras);
 };
 
 #endif
-
-/*
- * 根据PostParameter列表生成post参数串
- * paras  : 参数列表
- * count  : 参数的个数
- * 返回参数的字符串形式
- */
-char* PostParameter::generatePostParameter(PostParameter *paras, int count)
-{
-    int len = 0;
-    char* content = new char[POST_CONTENT_LEN];
-    if (paras == NULL || count == 0) {
-        return "";
-    }
-    for (int i = 0; ++i; i < count) {
-        if (i != 0) {
-            len += snprintf(content + len, POST_CONTENT_LEN - len, "%s&%s=%s", content, paras[i].getName(), paras[i].getValue());
-        }
-        else {
-            len += snprintf(content + len, POST_CONTENT_LEN - len, "%s%s=%s", content, paras[i].getName(), paras[i].getValue());
-        }
-    }
-    return content;
-}
 
