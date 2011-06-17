c398e7ca5240bf0443a818e121ddf11fd2e07acf
lvjin.buaa@gmail.com <lvjin.buaa@gmail.com@7c53731f-62f4-5332-f4eb-b6b0b6567c15>
2011-06-17 13:23:28 +0000

    build.sh by lvjin

---
+++ trunk/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+OBJ_DIRS=(HttpHelper OauthHelper Sina api)
+if [ $# -eq 0 ]
+then
+    for dir in ${OBJ_DIRS[@]}
+    do
+        cd $dir; echo "entry $dir";
+        make; 
+        cp -v lib*.so* ../lib;
+        cd -;
+    done;
+elif [ $1 = "clean" ]
+then
+    for dir in ${OBJ_DIRS[@]}
+    do  
+        cd $dir; echo "entry $dir";
+        make clean;
+        cd -;
+    done;
+else
+    echo "invalid option $1";
+fi
