5159f322bfebc545731f8d5b1ccbecc5a2684133
lvjin.buaa <lvjin.buaa@gmail.com>
2011-06-17 06:06:05 +0000

    lvjin

--- codeCounter.sh
+++ codeCounter.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#find ./ -name "*.cpp" -exec wc -l {} \; #| awk 'BEGIN{total=0} {total+=$1} END{print $total}';
+find ./ -name "*.cpp" -exec wc -l {} \; #| awk 'BEGIN{total=0} {total+=$1} END{print $total}';
 sum=0
 for count in `find ./ -name "*.cpp" -exec wc -l {} \; | awk '{print $1}'`
 do
     sum=$((sum+$count));
 done;
+echo ""
 echo "代码总行数:$sum"
