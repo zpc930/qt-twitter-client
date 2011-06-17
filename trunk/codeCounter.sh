c6fcc495d35be6cc00616d2c870085198fd7eefb
lvjin.buaa@gmail.com <lvjin.buaa@gmail.com@7c53731f-62f4-5332-f4eb-b6b0b6567c15>
2011-06-17 06:02:23 +0000

    add a code counter by lvjin

---
+++ trunk/codeCounter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#find ./ -name "*.cpp" -exec wc -l {} \; #| awk 'BEGIN{total=0} {total+=$1} END{print $total}';
+sum=0
+for count in `find ./ -name "*.cpp" -exec wc -l {} \; | awk '{print $1}'`
+do
+    sum=$((sum+$count));
+done;
+echo "代码总行数:$sum"
