5d72847650107f57994b649c2abaff13afc5493c
nieyaolong@gmail.com <nieyaolong@gmail.com@7c53731f-62f4-5332-f4eb-b6b0b6567c15>
2011-06-18 03:26:26 +0000

    compile problem

--- trunk/Sina/SinaApiProvider.cpp
+++ trunk/Sina/SinaApiProvider.cpp
@@ -8,3 +8,14 @@ SinaApiProvider::SinaApiProvider(OauthHelper *helper)
 {
     this->oauth = helper;
 }
+
+
+int MiniBlogProvider::OauthStep1(string * url)
+{
+    return this->oauth->LoginStep1(url);
+}
+
+int MiniBlogProvider::OauthStep2(string pin)
+{
+    return this->oauth->LoginStep2(pin);
+}
